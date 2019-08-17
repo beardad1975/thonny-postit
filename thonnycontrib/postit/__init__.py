0b0cca507d0e21ec8c35d19402872056252b7f1e
Wen-Hung , Chang <bearada1975@gmail.com>
2019-08-17 09:04:15 +0800

    try Positi  class

--- thonnycontrib/postit/__init__.py
+++ thonnycontrib/postit/__init__.py
@@ -2,23 +2,24 @@ from thonny import get_workbench, get_shell
 import tkinter as tk
 from tkinter import ttk
 
-class PostItView(ttk.Frame):
-    def __init__(self, master):
-        ttk.Frame.__init__(self, master)
-        # self.ent1 = tk.Entry(self)
-        # self.ent1.pack()
-        # self.ent2 = tk.Entry(self)
-        # self.ent2.pack()
-        
-        
-        self.post_button = ttk.Button(self, text='新增隨機方塊()', command=self.post)
-        self.post_button.pack(side=tk.TOP,anchor='w')
+#postit is  a frame with button and label
+class Postit(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.postit_button = tk.Button(self,  text='***' , command=self.post)
+        self.postit_button.pack(side=tk.LEFT, anchor='w')
 
+        self.help_label = tk.Label(self, text='...' )
+        self.help_label.pack(side=tk.LEFT, anchor='w')
 
         #right click menu
         self.popup_menu = tk.Menu(self, tearoff=0)
         self.popup_menu.add_command(label="修改", command=self.modify)
-        self.post_button.bind("<Button-3>", self.popup) # Button-2 on Aqua
+        self.postit_button.bind("<Button-3>", self.popup)
+
+    def set_content(self, text, help_text):
+        self.postit_button.config(text=text)
+        self.help_label.config(text=help_text)
 
     def popup(self, event):
         try:
@@ -37,15 +38,72 @@ class PostItView(ttk.Frame):
         editor = get_workbench().get_editor_notebook().get_current_editor()
         text = editor.get_text_widget()
         
+        text.see('insert')
         #check selection
 
         if len(text.tag_ranges('sel')):
             #replace selection 
             text.direct_delete(tk.SEL_FIRST, tk.SEL_LAST)
-            text.direct_insert("insert", self.post_button['text']) 
+            text.direct_insert("insert", self.postit_button['text']) 
         else:
             #just insert
-            text.direct_insert("insert", self.post_button['text'])
+            text.direct_insert("insert", self.postit_button['text'])
+
+
+
+class PostitView(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        p = Postit(self)
+        p.set_content('物理舞台', ' ... stage')
+        p.pack(side=tk.TOP, anchor='w', padx=5, pady=5)
+
+        Postit(self).pack(side=tk.TOP, anchor='w', padx=5, pady=5)
+
+
+    #     tk.Frame.__init__(self, master)
+    #     # self.ent1 = tk.Entry(self)
+    #     # self.ent1.pack()
+    #     # self.ent2 = tk.Entry(self)
+    #     # self.ent2.pack()
+        
+        
+    #     self.post_button = tk.Button(self, text='新增隨機方塊(x, y)12345', command=self.post)
+    #     #self.post_button.pack(anchor='w')
+    #     self.post_button.pack(anchor='w',padx=5, pady=5)
+
+    #     #right click menu
+    #     self.popup_menu = tk.Menu(self, tearoff=0)
+    #     self.popup_menu.add_command(label="修改", command=self.modify)
+    #     self.post_button.bind("<Button-3>", self.popup) # Button-2 on Aqua
+
+    # def popup(self, event):
+    #     try:
+    #         self.popup_menu.tk_popup(event.x_root, event.y_root)
+    #     finally:
+    #         self.popup_menu.grab_release()
+    
+
+    # def modify(self):
+    #     print('modify')
+
+    # def post(self):
+    #     #shell = get_shell()
+    #     #shell.submit_python_code(self.ent.get()+ '\n') 
+    #     #shell.focus_set()
+    #     editor = get_workbench().get_editor_notebook().get_current_editor()
+    #     text = editor.get_text_widget()
+        
+    #     text.see('insert')
+    #     #check selection
+
+    #     if len(text.tag_ranges('sel')):
+    #         #replace selection 
+    #         text.direct_delete(tk.SEL_FIRST, tk.SEL_LAST)
+    #         text.direct_insert("insert", self.post_button['text']) 
+    #     else:
+    #         #just insert
+    #         text.direct_insert("insert", self.post_button['text'])
 
 
 
@@ -83,4 +141,4 @@ class PostItView(ttk.Frame):
         #text.direct_insert("insert wordend", self.ent2.get())
 
 def load_plugin():
-    get_workbench().add_view(PostItView, '程式便利貼', 'ne')
+    get_workbench().add_view(PostitView, '程式便利貼', 'ne')
