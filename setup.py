597385b6799e418e37ed7fa3e202cf16bb09f214
Wen-Hung , Chang <bearada1975@gmail.com>
2020-06-07 22:17:41 +0800

    change setup.py info

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ setup (
         description="Program post-it for Thonny IDE",
         long_description="""Program post-it for Thonny IDE""",
         url="https://github.com/beardad1975/thonny-postit",
-        author="Wen-Hung, Chang",
+        author="Wen-Hung, Chang 張文宏",
         author_email="beardad1975@nmes.tyc.edu.tw",
         
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
             "Operating System :: Microsoft :: Windows",
-            "Operating System :: MacOS",
-            "Operating System :: POSIX :: Linux",
+            #"Operating System :: MacOS",
+            #"Operating System :: POSIX :: Linux",
             "Natural Language :: Chinese (Traditional)",
         ],
         
-        platforms=["Windows", "macOS", "Linux"],
+        platforms=["Windows"],
         python_requires=">=3.5",
         
-        install_requires=["thonny == 3.2.0"],
+        install_requires=["thonny == 3.2.0","pillow >= 7.1.2"],
         packages=["thonnycontrib.postit"],
 )
