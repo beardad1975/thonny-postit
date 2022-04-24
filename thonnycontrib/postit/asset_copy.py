888fe2bda5fbdb2f4653f8f7acd17da0ed409f1e
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2022-04-25 05:20:04 +0800

    basic asset group class structure

--- thonnycontrib/postit/asset_copy.py
+++ thonnycontrib/postit/asset_copy.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import tkinter.font as font
 from tkinter import ttk
+from collections import OrderedDict
+from pathlib import Path
+import json
+from PIL import Image, ImageTk
 
 from .common import common_images
 from . import common
@@ -13,14 +17,63 @@ class AssetGroup:
         self.asset_group = asset_group
         self.group_title = group_title
 
-        #print('in class: ', group_title)
+        #load asset cetegories
+        self.categories = OrderedDict()
+
+        categories_path = Path(__file__).parent / 'assets' / asset_group
+        
+        with open(categories_path / 'categories_info.json', encoding='utf8') as fp:
+            categories_info = json.load(fp)
+        
+        for c in categories_info:
+            asset_category = c['asset_category']
+            category_title = c['category_title']
+            self.categories[asset_category] = AssetCategory(asset_group,
+                                                        asset_category, category_title)
+
+        #print('categories: ', self.categories)
 
 class AssetCategory:
-    pass
+    def __init__(self, asset_group, asset_category, category_title):
+        self.asset_group = asset_group
+        self.asset_category = asset_category
+        self.category_title = category_title
 
+        #load asset cetegories
+        self.files = OrderedDict()
+
+        files_path = Path(__file__).parent / 'assets' / asset_group / asset_category
+        
+        with open(files_path / 'files_info.json', encoding='utf8') as fp:
+            files_info = json.load(fp)
+        
+        for f in files_info:
+            asset_file = f['asset_file']
+            thumbnail = f['thumbnail']
+            file_title = f['file_title']
+            file_type = f['file_type']
+            file_info = f['file_info']
+
+            self.files[asset_file] = AssetFile(asset_group, asset_category, 
+                                            asset_file, thumbnail, file_title,
+                                            file_type, file_info)
+        #print('files: ', self.files)
 
 class AssetFile:
-    pass
+    def __init__(self, asset_group, asset_category, asset_file, 
+                thumbnail, file_title, file_type, file_info):
+        self.asset_group = asset_group
+        self.asset_category = asset_category
+        self.asset_file = asset_file
+        self.thumbnail = thumbnail
+        self.file_title = file_title
+        self.file_type = file_type
+        self.file_info = file_info
+
+        # load thumbnail image
+        thumbnail_path = Path(__file__).parent / 'assets' / asset_group / asset_category /  thumbnail
+        _im = Image.open(thumbnail_path)       
+        self.thumbnail_img = ImageTk.PhotoImage(_im)
 
 
 class AssetCopyBtn(ttk.Button):
