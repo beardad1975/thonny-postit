7eea61722aa8f8f96d8239ff28aa8d6f2ff021a5
beardad1975 <beardad1975@nmes.tyc.edu.tw>
2023-11-22 17:53:18 +0800

    key fade effect working

--- thonnycontrib/postit/tools/keyin_display_tool_postit.py
+++ thonnycontrib/postit/tools/keyin_display_tool_postit.py
@@ -1,11 +1,15 @@
+import time
+
 import tkinter as tk
 from tkinter import ttk
 import tkinter.font as font
 
+from threading import Thread, Event
+from queue import Queue
+
 from thonny.codeview import CodeViewText
 from thonny.shell import ShellText
 from thonny import get_workbench, get_shell
-
 from .tool_postit import ToolWidget, ToolCodeMixin
 from ..base_postit import BaseCode, BasePost, BasePopup
 from .. import common
@@ -16,6 +20,7 @@ from .. import common
 import keyboardlayout
 from keyboardlayout import tkinter as kbl_tkinter
 from keyboardlayout.key import Key
+import keyboard
 
 grey = '#eeeeee'
 dark_grey = '#626262'
@@ -166,48 +171,110 @@ def get_keyboard(frame,
     
     return keyboard_widget   
 
+class AsyncKeyboardInput(Thread):
+    def __init__(self, keyin_display):
+        super().__init__()
+        self.keyin_display = keyin_display
+        self.last_key_event = None
+        
+    def run(self):
+        while self.keyin_display.keyin_thread_running_event.is_set() :
+            event = keyboard.read_event() 
+            if self.keyin_display.keyboard_visible_event.is_set() and \
+                self.keyin_display.is_focus_in_editor_or_shell() :
+                if self.last_key_event != event :
+                    #print('key event: ',event.event_type, event.name)
+                    self.keyin_display.key_event_queue.put(event)
+                    self.last_key_event = event
+                else:
+                    pass
+                    #print('repeat keyin : ignore')
+            else:
+                pass
+                #print("keyboard invisible : ignore")
+        print('keyin thread stop')    
+         
+
+
 class KeyinDisplayToolWidget:
 
     def keyin_display_init(self, keyin_display_frame):
+        """
+        keyin thread started when first click tool-icon. then last to the end.
+        """
         self.keyin_display_frame = keyin_display_frame
-        self.is_keyboard_visible = True
         self.is_keyin_playing = False
-        self.keyin_playing_cycle = []
+        self.keyin_playing_cycle = list()
+        self.var_is_anchored_top = True
+        self.key_event_queue = Queue()
+        self.keyin_thread_running_event = Event() # thread event : default clear(false)
 
-        self.var_is_anchored_top = tk.BooleanVar()
-        self.var_is_anchored_top.set(1)
+        self.is_fading = True
+        self.fade_key_set = set()
+        self.fade_key_time_dict = dict()
+        self.startup_time = time.time() # time dict default value
+        self.fade_total_seconds = 1
+        self.fade_levels_num = 2
+        self.fade_time_unit = self.fade_total_seconds/self.fade_levels_num
+        self.fade_grey_lowbound = 150
+        self.fade_grey_upbound = 220
+        self.fade_grey_unit = (self.fade_grey_upbound - self.fade_grey_lowbound
+                               ) / self.fade_levels_num  
 
-        key_info = keyboardlayout.KeyInfo(
-        margin=3,
-        color=grey,
-        txt_color=dark_grey,
-        txt_font=font.Font(family='Arial', size=6),
-        txt_padding=(1, 1))
+        # print('---------------')
+        # print(self.startup_time)
+        # print(self.fade_time_unit)
+        # print(self.fade_grey_unit)
 
-        pressed_key_info = keyboardlayout.KeyInfo(
-        margin=3,
-        color='red',
-        txt_color='white',
-        txt_font=font.Font(family='Arial', size=6),
-        txt_padding=(1,1))
+        # normal key_info
+        self.normal_key_info = keyboardlayout.KeyInfo(
+            margin=3,
+            color=grey,
+            txt_color=dark_grey,
+            txt_font=font.Font(family='Arial', size=6),
+            txt_padding=(1, 1))
 
-        # already packed inside 
+        # pressed key_info (red)
+        self.pressed_key_info = keyboardlayout.KeyInfo(
+            margin=3,
+            color='red',
+            txt_color='white',
+            txt_font=font.Font(family='Arial', size=6),
+            txt_padding=(1,1))
+
+        # generate fade_key_info
+        self.fade_keyinfo_list = []
+        for i in range(self.fade_levels_num):
+            c = self.fade_grey_lowbound + int(self.fade_grey_unit * i)
+            color_string = '#%02x%02x%02x' % (c, c, c)
+            keyinfo =  keyboardlayout.KeyInfo(
+                            margin=3,
+                            color=color_string,
+                            txt_color=dark_grey,
+                            txt_font=font.Font(family='Arial', size=6),
+                            txt_padding=(1, 1))
+            self.fade_keyinfo_list.append(keyinfo)
+
+        #  packed inside library 
         self.keyin_display = get_keyboard(keyin_display_frame,
-               keyboardlayout.LayoutName.QWERTY, key_info)
+               keyboardlayout.LayoutName.QWERTY, self.normal_key_info)
         
-        # switch keyin display status
-        self.switch_keyin_display()
-    
+        #  keyin display status default invisible
+        self.keyboard_visible_event = Event() # thread event : default clear(false)
+        self.keyin_display_frame.pack_forget()
+
+
     def switch_keyin_display(self):
-        if self.is_keyboard_visible:
-            
+        if self.keyboard_visible_event.is_set(): 
             self.keyin_display_frame.pack_forget()
-            #self.keyin_display.grid_remove()
-            self.is_keyboard_visible = False
+            self.keyboard_visible_event.clear()
             #restore button
             self.postit_button.config(bg='SystemButtonFace')
-        else:
-            if self.var_is_anchored_top.get():
+        else: 
+            self.keyboard_visible_event.set()
+            self.monitor_keyin()
+
+            if self.var_is_anchored_top:
                 # re-pack keyin_display and notebook_frame
                 current_mode = common.postit_view.current_mode
                 common.postit_view.all_modes[current_mode].notebook_frame.pack_forget()
@@ -218,9 +285,74 @@ class KeyinDisplayToolWidget:
             else:
                 self.keyin_display_frame.pack(side=tk.TOP, fill=tk.X)
 
-            self.is_keyboard_visible = True
             # highlight button
             self.postit_button.config(bg='green')
+    
+    def monitor_keyin(self):
+        if self.keyin_thread.is_alive():
+            # check the thread regularly
+            if self.keyboard_visible_event.is_set():
+                self.after(100, lambda: self.monitor_keyin())
+            
+            self.key_fade_effect()
+
+            if not self.key_event_queue.empty():
+                event = self.key_event_queue.get()
+                key = key_dict.get(event.name)
+                if key is None:
+                    print('no key input mapping')
+                    return
+                # print(key)
+                self.key_press_effect(event.event_type, key)
+
+                 
+
+    def key_press_effect(self, key_type, key):
+            if key_type == 'down':
+                self.keyin_display.update_key(key, self.pressed_key_info)
+            elif key_type == 'up':
+                if self.is_fading:
+                    print('add key set and dict: ', key)
+                    self.fade_key_set.add(key)
+                    self.fade_key_time_dict[key] = time.time()
+                else:
+                    self.keyin_display.update_key(key, self.normal_key_info)
+
+    def key_fade_effect(self):
+        if not self.is_fading:
+            return
+        
+        current_time = time.time()
+        overdue_list = list()
+
+        # fade key 
+        for key in self.fade_key_set:
+            pressed_time = self.fade_key_time_dict.get(key, self.startup_time)
+            if pressed_time + self.fade_total_seconds <= current_time:
+                # overdue
+                overdue_list.append(key)
+                continue
+            
+            # fade effect
+            fade_level = int((current_time - pressed_time) / self.fade_time_unit) 
+            keyinfo = self.fade_keyinfo_list[fade_level]
+            self.keyin_display.update_key(key, keyinfo) 
+
+        # overdue key process
+        for key in overdue_list:
+            self.fade_key_set.remove(key)
+            self.keyin_display.update_key(key, self.normal_key_info)
+
+
+
+    def is_focus_in_editor_or_shell(self):
+        workbench = get_workbench()
+        focus_widget = workbench.focus_get()
+        if isinstance(focus_widget, CodeViewText) or isinstance(focus_widget, ShellText):
+            return True
+        else:
+            return False
+          
 
 # class EnterToolPostMixin:
 #     def insert_into_editor(self, editor_text, 
@@ -260,7 +392,23 @@ class KeyinDisplayToolPostMixin:
         self.postit_button.bind("<Button-1>", self.on_mouse_release)
         #self.postit_button.config(cursor='arrow')
 
+    def start_thread(self):
+        self.keyin_thread = AsyncKeyboardInput(self)
+        self.keyin_thread_running_event.set()
+        self.keyin_thread.start()
+
+        get_workbench().bind("WorkbenchClose", self._stop_keyin_thread, True)
+
+        print("keyin thread started")
+
+    def _stop_keyin_thread(self, event):
+        self.keyin_thread_running_event.clear()
+        print("stop keyin thread")
+
     def on_mouse_release(self, event):
+        if not self.keyin_thread_running_event.is_set():
+            self.start_thread()
+
         self.switch_keyin_display()
 
     # def insert_into_editor(self, editor_text, 
@@ -325,19 +473,20 @@ class KeyinDisplayToolPopup:
 
     def rearrange_keyin_display(self, top_pos):
         if top_pos:
-            self.var_is_anchored_top.set(1)
+            self.var_is_anchored_top = True
         else:
-            self.var_is_anchored_top.set(0)
+            self.var_is_anchored_top = False
 
 
-        if not self.is_keyboard_visible:
+        if not self.keyboard_visible_event.is_set():
             # highlight button
             self.postit_button.config(bg='green')
-            self.is_keyboard_visible = True
+            self.keyboard_visible_event.set()
+            self.monitor_keyin()
 
         self.keyin_display_frame.pack_forget()
 
-        if self.var_is_anchored_top.get():
+        if self.var_is_anchored_top:
             # re-pack keyin_display and notebook_frame
             current_mode = common.postit_view.current_mode
             common.postit_view.all_modes[current_mode].notebook_frame.pack_forget()
